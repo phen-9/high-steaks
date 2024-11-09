800f310f6cc93c5f7821ae5a76fdf97198dd74fa
phen-9 <gavinpuzon@gmail.com>
2024-11-08 21:11:30 -0800

    Add files via upload

---
+++ Resources/velocity-component.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var x : int
+	var y : int
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
