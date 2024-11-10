73070361b0e2443c4e03177927925f6e629ab30f
VortexCoreCoding <logan.spinali@wsu.edu>
2024-11-10 13:16:06 -0800

    Stove SFX

--- Entities/stove/stove.gd
+++ Entities/stove/stove.gd
@@ -2,9 +2,10 @@ extends AnimatableBody2D
 
 @onready var sprite = $Sprite
 @onready var timer = $Timer
-@onready var hitbox = $Hitbox
 const HITBOX_PREFAB = preload("res://Entities/stove/hitbox.tscn")
 
+@export var tick : AudioStreamPlayer2D
+@export var fwoosh : AudioStreamPlayer2D
 @export var duration := 3
 
 func _ready() -> void:
@@ -12,7 +13,13 @@ func _ready() -> void:
 	timer.start(duration)
 	pass
 
+func _process(delta: float) -> void:
+	if(!tick.playing):
+		tick.play()
+	pass
+
 func _on_timer_timeout() -> void:
+	fwoosh.play()
 	sprite.animation = "blast"
 	sprite.play()
 	var hitbox = HITBOX_PREFAB.instantiate()
