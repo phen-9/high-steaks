836af8c399263fef0250c5d42e100670b01dfd3e
phen-9 <gavinpuzon@gmail.com>
2024-11-10 18:34:03 -0800

    yippee

--- Scenes/player_handler.gd
+++ Scenes/player_handler.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 const PLAYER_PREFAB = preload("res://Entities/player/player.tscn")
 @onready var player = $Player
+var level = 0
 signal clear_sauce
+
+var levels : Dictionary = {0 : preload("res://Scenes/Levels/tutorial.tscn")}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +15,9 @@ func _ready() -> void:
 		var child = children[i]
 		if(child.has_method("reactivate_sauce")):
 			clear_sauce.connect(child.reactivate_sauce)
-		
+		if(child.has_signal("finish_level")):
+			child.finish_level.connect(finish_level)
+	
 	
 	pass # Replace with function body.
 
@@ -26,4 +31,8 @@ func spawn_player():
 	var new_player = PLAYER_PREFAB.instantiate()
 	new_player.death.connect(spawn_player)
 	add_child(new_player)
+	pass
+
+func finish_level():
+	print("YIPPEE")
 	pass
