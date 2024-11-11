70362920a392e3ed87dc4b8e90e7499e6899acdd
phen-9 <gavinpuzon@gmail.com>
2024-11-10 16:25:44 -0800

    knife fix :)

--- Entities/knife/knife.gd
+++ Entities/knife/knife.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-@export var follow_speed := 30
+
 @onready var knife_body = $KnifeBody
-@onready var sprite = $KnifeBody/Sprite2D
-@onready var search_area = $SearchArea
-@onready var timer = $Timer
-@onready var return_timer = $ReturnTimer
+
 
 # 0 for wait, 1 for murder, 2 for return
 var return_y
